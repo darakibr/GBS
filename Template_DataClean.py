270307712580659e0faef0a60f7cc7850987c12d
Daniel <darakibr@gmail.com>
2022-02-28 15:25:12 -0600

    Update Template_DataClean.py

--- Template_DataClean.py
+++ Template_DataClean.py
@@ -11,11 +11,28 @@ df = df.replace('.', np.nan) # Programs use '.' placeholder for missing values
   meta = pd.read_csv( '<path_to_meta_file>' )
   dfmerged =  pd.concat([meta, df], join='outer', axis=1, verify_integrity=True)
   
-  count = dfmerged.append(dfmerged.count().rename('Total')) # Adds a row with the total number of isolates per category (snp)
-  n = len(count)-2 # Total number of isolates, subtract 2 because first 2 rows contain meta data associated with columns
-  percent = count.append([count.iloc[n,:]/n*100].rename('Percent'))
   
+count = dfmerged.append(dfmerged.count().rename('Total')) # Adds a row with the total number of isolates per category (snp)
+n = len(count)-2 # Total number of isolates, subtract 2 because first 2 rows contain meta data associated with columns
+percent = count.append([count.iloc[n,:]/n*100].rename('Percent'))
   
+percent.to_csv(' <path_to_METAwTotals.csv> ' )
   
-  percent.to_csv(' <path_to_METAwTotals.csv> ' )
-  
+### EXAMPLE 2, working without transposing first
+
+df = pd.read_csv( '<path_to_file>' , sep='\t', index_col=0, header=0)
+def search_column(df,searchcol, val, new_col = True, newname=0):
+  if new_col == True:
+    if newname ==0:
+      newname = input('New Column Name: ')
+    list = df[searchcol]
+    df[newname] = [1 if val in row else np.nan for row in list] # iterates over rows adding a 1 if val found, otherwise adding NaN
+      
+def total(data, add_percent = True):
+  temp = data.transpose()
+  data['Total'] = data.count()
+  if add_percent == True:
+    n = len(data)-23 ### There are 23 descriptive rows before individual isolates are listed, other vcf files may vary. ###
+    data['Percent'] = data['Total']/n*100
+  return data.transpose()
+
